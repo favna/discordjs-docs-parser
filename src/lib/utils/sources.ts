c9fe46d1f4aa5162f50faecd87e44aa365d9417b
Jeroen Claassens <support@favware.tech>
2021-11-24 22:24:43 +0100

    fix: fixed rpc repo url

--- src/lib/utils/sources.ts
+++ src/lib/utils/sources.ts
@@ -6,7 +6,7 @@ import type { SourcesStringUnion } from './interfaces';
 export const sources = new Map<SourcesStringUnion, string>([
   ['stable', 'https://raw.githubusercontent.com/discordjs/discord.js/docs/stable.json'],
   ['main', 'https://raw.githubusercontent.com/discordjs/discord.js/docs/main.json'],
-  ['rpc', 'https://raw.githubusercontent.com/discordjs/RPC/docs/master.json'],
+  ['rpc', 'https://raw.githubusercontent.com/discordjs/rpc/docs/master.json'],
   ['collection', 'https://raw.githubusercontent.com/discordjs/collection/docs/stable.json'],
   ['builders', 'https://raw.githubusercontent.com/discordjs/builders/docs/stable.json'],
   ['voice', 'https://raw.githubusercontent.com/discordjs/voice/docs/stable.json']
