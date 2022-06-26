cbacfa9d4df43c2da89aea5a9e7b7cf97a949d7c
Jeroen Claassens <support@favware.tech>
2022-06-26 16:15:36 +0200

    chore: yep, opatut was right on this one.
    permalink: http://whatthecommit.com/afbe88f970b36d9b7e3a11f84bf37670

--- tests/all-fields.test.ts
+++ tests/all-fields.test.ts
@@ -219,7 +219,7 @@ describe('All fields', () => {
 
     expect(clientReady?.url).toBe('https://discord.js.org/#/docs/discord.js/main/class/Client?scrollTo=e-ready');
     expect(clientReady?.sourceURL).toBe(
-      'https://github.com/discordjs/discord.js/tree/main/packages/discord.js/src/client/websocket/WebSocketManager.js#L378'
+      'https://github.com/discordjs/discord.js/tree/main/packages/discord.js/src/client/websocket/WebSocketManager.js#L380'
     );
     expect(clientReady?.link).toBe('[Client#ready](https://discord.js.org/#/docs/discord.js/main/class/Client?scrollTo=e-ready)');
 
